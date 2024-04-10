9562b15368691b7b69cd5e89345cade4f1ad8e43
Leon Sokolowski <leonsokolowski2104@gmail.com>
2024-04-10 16:51:59 -0300

    Ejercicios C#

---
+++ Ejercicios C#/Ejercicio N°1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°1";
+            // Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+
+            int numeroIngresado = 0;
+            int numeroMayor = 0;
+            int numeroMenor = 0;
+            int sumaNumeros = 0;
+            float promedio;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                numeroIngresado = int.Parse(Console.ReadLine());
+                sumaNumeros += numeroIngresado;
+
+                if (i == 0)
+                {
+                    numeroMayor = numeroIngresado;
+                    numeroMenor = numeroIngresado;
+                }
+                else
+                {
+                    if (numeroIngresado > numeroMayor)
+                    {
+                        numeroMayor = numeroIngresado;
+                    }
+                    else if (numeroIngresado < numeroMenor)
+                    {
+                        numeroMenor = numeroIngresado;
+                    }
+                }
+                
+            }
+            promedio = (float)(sumaNumeros) / 5;
+
+            Console.WriteLine("\nEl numero mayor es: {0}", numeroMayor);
+            Console.WriteLine("El numero menor {0}", numeroMenor);
+            Console.WriteLine("El promedio es {0:#,###.00}", promedio);
+            Console.Write("\nOprima cualquier boton para concluir");
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Ejercicios C#/Ejercicio N°2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°2";
+            //Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+            double numeroIngresado;
+            
+            do
+            {
+                Console.WriteLine("Ingrese un número: ");
+                numeroIngresado = double.Parse(Console.ReadLine());
+
+                if (numeroIngresado <= 0 )
+                {
+                    Console.WriteLine("ERROR.¡Reingresar número!: ");
+                    numeroIngresado = double.Parse(Console.ReadLine());
+                }
+            } while (numeroIngresado <= 0);
+           
+
+            double numeroCuadrado = (double)Math.Pow(numeroIngresado, 2);
+            double numeroCubico = (double)Math.Pow(numeroIngresado, 3);
+
+            Console.WriteLine("El cuadrado de {0:#,###.00} es {1:#,###.00} y el cubo es {2:#,###.00}", numeroIngresado, numeroCuadrado, numeroCubico);
+            Console.Write("\nOprima cualquier boton para concluir");
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
