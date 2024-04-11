40fe31660a898b14109f61f9caa43a596548d8cc
leonsokolowski <leonsokolowski2104@gmail.com>
2024-04-10 21:11:11 -0300

    Clase 2

---
+++ Ejercicios C#/Ejercicios Clase 1/Ejercicio N°3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°3";
+            /*Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el
+            usuario por consola.
+            Validar que el dato ingresado por el usuario sea un número.
+            Volver a pedir el dato hasta que sea válido o el usuario ingrese "salir".
+            Si ingresa "salir", cierra la consola.
+            Al finalizar, preguntar al usuario si desea volver a operar.Si la respuesta es afirmativa,
+            iterar.De lo contrario, cerrar la consola.
+            */
+
+            int numeroIngresado;
+
+            /*do
+            {
+                Console.WriteLine("Ingrese un numero: ");
+
+            }*/
+        }
+    }
+}
