75bbdd6499cb7b6951bb9140678934bd1791f42c
leonsokolowski <leonsokolowski2104@gmail.com>
2024-04-15 22:02:22 -0300

    15/4

---
+++ Ejercicios C#/Ejercicios Clase 3/BIblioteca Clase 3/Clases Ejercicio 1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Clase_3
+{
+    //----------------------------------------------------------------------------------------------------------------
+    //Ejercicio 1
+    public class Cuenta
+    {
+        #region Atributos
+        private string titular;
+        private decimal cantidad;
+        #endregion
+
+        #region Metodos
+        public Cuenta(string titular, decimal cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string DevolverTitular()
+        {
+            return this.titular;
+        }
+
+        public decimal DevolverCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string Mostrar()
+        {
+            return $"-----------------------------------------\nTitular = {this.titular}\nCantidad = {this.cantidad}\n-----------------------------------------\n";
+        }
+
+        public void Ingresar(decimal monto)
+        {
+            if (monto > 0)
+            {
+                this.cantidad += monto;
+                Console.WriteLine("Transferencia recibida");
+            }
+        }
+
+        public void Retirar(decimal monto)
+        {
+            if (monto > 0)
+            {
+                this.cantidad -= monto;
+            }
+
+        }
+        #endregion
+    }
+    //----------------------------------------------------------------------------------------------------------------
+    //Ejercicio 2
+    public class Persona
+    {
+        #region Atributos
+        private string nombre;
+        private DateTime fechaNacimiento;
+        private int dni;
+        #endregion
+
+        #region Metodos
+        public Persona(string nombre, DateTime fechaNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+            this.dni = dni;
+        }
+
+
+        #endregion
+    }
+
+}
---
+++ Ejercicios C#/Ejercicios Clase 3/Ejercicio N°1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca_Clase_3;
+
+namespace Ejercicio_N_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°1";
+
+            //Primer ejemplo
+            Cuenta primeraCuenta = new Cuenta("Leon", 500);
+
+            Console.WriteLine(primeraCuenta.Mostrar());
+
+            primeraCuenta.Ingresar(200);
+            Console.WriteLine(primeraCuenta.Mostrar());
+
+            primeraCuenta.Retirar(200);
+            Console.WriteLine(primeraCuenta.Mostrar());
+
+            //Segundo ejemplo
+            Cuenta segundaCuenta = new Cuenta("Rocio", 1000);
+
+            Console.WriteLine(segundaCuenta.Mostrar());
+
+            segundaCuenta.Ingresar(10000);
+            Console.WriteLine(segundaCuenta.Mostrar());
+
+            segundaCuenta.Retirar(6000);
+            Console.WriteLine(segundaCuenta.Mostrar());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
