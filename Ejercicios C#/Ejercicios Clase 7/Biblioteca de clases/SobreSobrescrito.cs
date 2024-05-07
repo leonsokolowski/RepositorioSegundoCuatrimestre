49429b3b4c0c9e76b29bfbe5b584edbb0ad37af1
leonsokolowski <leonsokolowski2104@gmail.com>
2024-05-07 00:20:03 -0300

    Clase 7

---
+++ Ejercicios C#/Ejercicios Clase 7/Biblioteca de clases/SobreSobrescrito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_clases
+{
+    public class SobreSobrescrito : Sobrescrito
+    {
+        #region Propiedades
+        public override string miPropiedad { get { return miAtributo; } }
+        #endregion
+
+        #region Metodos
+        public override string MiMetodo()
+        {
+            return miPropiedad;
+        }
+        #endregion
+    }
+}
